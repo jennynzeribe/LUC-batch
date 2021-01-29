42f4097931614911bceb719817bae2fac01f413a
jennynzeribe <nzeribejennifer@gmail.com>
2021-01-29 12:36:41 +0100

    added a new tab

---
+++ index.js
@@ -0,0 +1,13 @@
+    // document.querySelector('#hey').addEventListener('click', onclick);
+
+    // function onClick(e){
+    //     alert('click');
+    // }
+
+    document.querySelector('#hey').addEventListener('click', function(){
+        console.log('hello world');
+    });
+
+    document.querySelector('#From').addEventListener('click', function(){
+            return new Date;
+    });
