f557e4c550f270abc622a830c3f3aa23345e73ca
jennynzeribe <nzeribejennifer@gmail.com>
2021-02-01 09:23:48 +0100

    stimulation of the progress bar

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-    // document.querySelector('#hey').addEventListener('click', onclick);
+let progressbar = document.querySelector('#progressbar');
+let progress = document.querySelector('#progress');
 
-    // function onClick(e){
-    //     alert('click');
-    // }
+let seconds = 50100/60;
 
-    document.querySelector('#hey').addEventListener('click', function(){
-        console.log('hello world');
-    });
+let timer;
 
-    document.querySelector('#From').addEventListener('click', function(){
-            return new Date;
-    });
+ let year = 100;
+ let width = 5;
+ let counter =60;
+
+timer = setInterval(() => {
+
+    
+    width+=1.6;
+    year += seconds;
+    progress.innerHTML = year;
+    progressbar.style.width=width+"%";   
+    counter+=6;
+    console.log(counter)
+    if(counter == 420){
+        clearInterval(timer);
+        console.log("Here oooo");
+    } 
+}, seconds);
+// var name = "Jenifer";
+
+// const functiovarai = function(){
+//     this.name ="Lolade";
+//     this.age =22;
+//     console.log(this)
+// }
+
+// const isFullname = ()=>{
+//     this.name ="Lolade";
+//     this.age =22;
+//     console.log(this);
+// }
+
+// functiovarai();
+
+// isFullname();
+
+// functiovarai =function(){
+//     console.log("Just testing");
+// }
+
+// functiovarai();
